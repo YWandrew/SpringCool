ed288aaec668648c7cd2892bf9442fa97ba63e7c
YWandrew <xjhtyw@gmail.com>
2018-06-05 13:38:14 +0800

    2018年6月5日 13点37分  ：changes web test controler

--- src/main/java/com/test/demo/web/TestController.java
+++ src/main/java/com/test/demo/web/TestController.java
@@ -3,7 +3,9 @@ package com.test.demo.web;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -29,5 +31,20 @@ public class TestController {
         result.put("3232r3","this a map ");
 
         return  result;
+    }
+
+
+    @GetMapping("testls")
+    public List<Map<String,Object>> getTheTestls(){
+        List<Map<String,Object>>  ls =  new ArrayList<Map<String, Object>>();
+        Map<String,Object> result = new HashMap<String, Object>();
+        result.put("test","this a map ");
+        result.put("test2","this a map ");
+        result.put("32","this a map ");
+        result.put("3232","this a map ");
+        result.put("te23232st","this a map ");
+        result.put("3232r3","this a map ");
+        ls.add(result);
+        return  ls;
     }
 }
